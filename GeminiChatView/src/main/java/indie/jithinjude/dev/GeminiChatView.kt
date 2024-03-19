8ea276fbee20fe29d9f4a48cc6c1c3f0491ab692
jithin825427 <jithin@thecalmsleep.com>
2024-03-20 01:25:08 +0530

    scroll fixes

--- GeminiChatView/src/main/java/indie/jithinjude/dev/GeminiChatView.kt
+++ GeminiChatView/src/main/java/indie/jithinjude/dev/GeminiChatView.kt
@@ -1,15 +1,18 @@
 package indie.jithinjude.dev
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.Send
@@ -21,6 +24,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
@@ -28,14 +32,12 @@ import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.google.ai.client.generativeai.GenerativeModel
-import com.google.ai.client.generativeai.type.content
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.android.awaitFrame
 import kotlinx.coroutines.launch
 
 @Composable
-fun GeminiChatView(apiKey: String){
+fun GeminiChatView(apiKey: String) {
+    val lazyColumnListState = rememberLazyListState()
+    val coroutineScope = rememberCoroutineScope()
     val chatDataList = remember { mutableStateOf(listOf<ChatMember>()) }
 
     val generativeModel = GenerativeModel(
@@ -43,24 +45,43 @@ fun GeminiChatView(apiKey: String){
         apiKey = apiKey
     )
     val chat = generativeModel.startChat()
-
-    Column(modifier = Modifier.fillMaxSize()) {
+    Box(modifier = Modifier.fillMaxSize()) {
         LazyColumn(
-            modifier = Modifier.weight(1f)){
-            items(chatDataList.value){ chat ->
-                Column(modifier = Modifier.fillMaxWidth().padding(20.dp)) {
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(bottom = 80.dp),
+            state = lazyColumnListState
+        ) {
+            items(chatDataList.value) { chat ->
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(20.dp)
+                ) {
                     Text(text = chat.text, fontSize = 20.sp)
                 }
             }
         }
-        Spacer(modifier = Modifier.height(20.dp))
         RoundedCornerTextFieldWithSend(
-            modifier = Modifier.fillMaxWidth(),
+            modifier = Modifier
+                .fillMaxWidth()
+                .align(Alignment.BottomCenter),
             onSendClick = {
-                CoroutineScope(Dispatchers.IO).launch {
+                coroutineScope.launch {
                     chatDataList.value += listOf(ChatMember(MemberType.USER, it))
-                    val response = chat.sendMessage(it)
-                    chatDataList.value += listOf(ChatMember(MemberType.BOT, response.text?:""))
+                    lazyColumnListState.animateScrollToItem(chatDataList.value.size - 1)
+                    try {
+                        val response = chat.sendMessage(it)
+                        chatDataList.value += listOf(
+                            ChatMember(
+                                MemberType.BOT,
+                                response.text ?: ""
+                            )
+                        )
+                        lazyColumnListState.animateScrollToItem(chatDataList.value.size - 1)
+                    } catch (ex: Exception) {
+                        ex.printStackTrace()
+                    }
                 }
             }
         )
@@ -80,16 +101,20 @@ fun RoundedCornerTextFieldWithSend(
             value = textState.value,
             onValueChange = { textState.value = it },
             label = { Text("Let's chat") },
-            modifier = Modifier.weight(1f).focusRequester(focusRequester), // Make text field occupy most space
+            modifier = Modifier
+                .weight(1f)
+                .focusRequester(focusRequester), // Make text field occupy most space
             shape = RoundedCornerShape(12.dp)
         )
         Spacer(modifier = Modifier.width(8.dp)) // Add some spacing between text field and button
         Button(
+            modifier = Modifier.size(50.dp),
+            contentPadding = PaddingValues(0.dp),
             onClick = {
                 onSendClick(textState.value)
                 textState.value = ""
-                      },
-            shape = RoundedCornerShape(12.dp) // Maintain consistent corner radius
+            },
+            shape = RoundedCornerShape(100.dp) // Maintain consistent corner radius
         ) {
             Icon(
                 imageVector = Icons.AutoMirrored.Filled.Send, // Replace with your send icon
